4f6dbb6f89225d1bd1644d8f87a22a47510bd805
ccy <princedevera10@yahoo.com>
2022-06-14 21:42:33 +0900

    comment out

--- pages/github/index.tsx
+++ pages/github/index.tsx
@@ -1,9 +1,8 @@
 import Layout from "../../components/layout/Layout"
 import { GetServerSideProps } from "next/types"
-import { xml2json } from "xml-js"
 import Timeline from "../../components/Timeline"
 
-const Github = ({ result }: { result: string }) => {
+const Github = ({ props }: { props: string }) => {
   return (
     <Layout title="Github" subtitle="Github RSS feed">
       <Timeline />
@@ -12,11 +11,11 @@ const Github = ({ result }: { result: string }) => {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const res: Response = await fetch("https://github.com/pdevera15.atom")
-  const body = await res.text()
-  const result = xml2json(body)
+  // const res: Response = await fetch("https://github.com/pdevera15.atom")
+  // const body = await res.text()
+  // const result = xml2json(body)
   return {
-    props: { result },
+    props: {},
   }
 }
 
