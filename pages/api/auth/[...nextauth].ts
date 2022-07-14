7642d3a1be1d367e550eaacdd8fd5567e6767567
princecyrelle <princedevera10@yahoo.com>
2022-07-15 00:21:36 +0900

    adding prisma

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,16 @@
 import NextAuth from "next-auth/next"
 import GithubProvider from "next-auth/providers/github"
+import { PrismaClient } from "@prisma/client"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+
+const prisma = new PrismaClient()
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
 })
--- pages/guestbook/index.tsx
+++ pages/guestbook/index.tsx
@@ -1,23 +1,28 @@
+import Layout from "../../src/components/layout/Layout"
 import { NextPage } from "next"
 import { useSession, signIn } from "next-auth/react"
-import Link from "next/link"
-import Github from "../github"
+import styled from "styled-components"
+
+const GuestbookWrapper = styled.div`
+  background-color: #424242;
+  margin: auto;
+  border-radius: 10px;
+  padding: 1em;
+  line-height: 2em;
+`
 
 const GuestBook: NextPage = () => {
   const { data, status } = useSession()
-  console.log(data, status)
   return (
-    <div>
-      {status === "unauthenticated" && (
-        <button
-          onClick={() => {
-            signIn()
-          }}
-        >
-          LOGIN
-        </button>
-      )}
-    </div>
+    <Layout
+      title="My github timeline"
+      subtitle="Leave some comment. Surprise me!"
+    >
+      <GuestbookWrapper>
+        Sign in using Github <br />
+        Your information is only used to display your name
+      </GuestbookWrapper>
+    </Layout>
   )
 }
 
